f677bc88e71a72d284473babe00e878364e0c76d
Noori <93557885+loveitall@users.noreply.github.com>
2023-02-05 04:28:04 +0900

    [bj2309] 일곱 난쟁이

--- string/bj2309.py
+++ string/bj2309.py
@@ -1,20 +1,19 @@
 # 일곱난쟁이
 
-N = map(int, input())
 arr = [] 
 for i in range(9):
     arr.append(int(input()))
 
 arr.sort()    
 
-total = sum(array)
+total = sum(arr)
 
 for i in range(len(arr)):
     for j in range(i+1, len(arr)) :
-        if total - (array[i] - array[j]) == 100 :
+        if total - arr[i] - arr[j] == 100 :
             for k in range(len(arr)) :
                 if k == i or k == j :
                     pass
                 else :
                     print(arr[k])
-                exit()
+            exit()
