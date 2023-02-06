404f4b1a663eba92c7648374857ae02eb0dfafff
Noori <93557885+loveitall@users.noreply.github.com>
2023-02-06 22:52:47 +0900

    [bj17608] 막대기

---
+++ stack/bj17608.py
@@ -0,0 +1,19 @@
+# 17608번 - 막대기
+
+import sys
+n = int(sys.stdin.readline())
+stack = [int(sys.stdin.readline()) for _ in range(n)]
+before = stack.pop()
+count = 1
+
+for i in range(1, n) :
+    last = stack.pop()
+
+    if last > before :
+        count += 1
+        before = last
+
+print(count)
+
+
+
