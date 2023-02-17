69976ff0ca3fe30573f7d97189438b06a771a165
Noori <93557885+loveitall@users.noreply.github.com>
2023-02-17 15:44:35 +0900

    [bj1 1388] 바닥 장식
    dfs/bfs로 풀지 못했는데...

---
+++ DFS,BFS/bj1388.py
@@ -0,0 +1,32 @@
+# 1388번 - 바닥 장식
+
+import sys
+input = sys.stdin.readline
+n, m = map(int, input().split())
+
+floor = []
+
+# 행렬 받기
+for _ in range(n) : 
+    floor.append(list(input()))
+
+count = 0
+
+# '-' 탐색
+for i in range(n) :
+    pre = "/"
+    for j in range(m) :
+        if floor[i][j] == "-" :
+            if floor[i][j] != pre : count += 1
+        pre = floor[i][j]
+
+# '|' 탐색
+for j in range(m) :
+    pre = "/"
+    for i in range(n) :
+        if floor[i][j] == "|" :
+            if floor[i][j] != pre : count += 1
+        pre = floor[i][j]
+
+# 갯수 출력
+print(count)
